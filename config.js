8848a20e5125622196e9b61e24e2dbff5ff5a717
FolexDev <alex@mediaonegroup.ch>
2026-06-23 10:00:17 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const CLIENT_ID='254325008139-rp0p175m2icjv0pr6jv9vp52n9jjpdik.apps.googleusercontent.com';
+const API_KEY='AIzaSyBw5-W46qmpeWn79XKtiqE1CZuiXXkHWH8';
+const WEBHOOK_URL='https://hook.eu2.make.com/i0u8f5djq5ed8ywae2eziznppk24pf9v';
+const ALLOWED=['alex@mediaonegroup.ch','lionel@mediaonegroup.ch'];
+const SHEETS=[
+  {id:'15GQ6osEPy44nmQw6HVD-L2UNEGQ5gCo8fow4YnxyY7c',name:'DISTRIBUTION ONE & LAC',panel:0},
+  {id:'116K_qpndEqkmOwIuvTBoiqtJG_v8UaFBsfNmg8RTxZ8',name:'DISTRIBUTION Carac-MOC',panel:1},
+  {id:'1J8833aM0abL9iTD0-GGMGlnkPc5vkqHRDXpvkWR8jrk',name:'DISTRIBUTION LFM & ROUGE',panel:2}
+];
